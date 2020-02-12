33da18d06ed23ea8dde1b64d95660c56acfcaec4
Matthew Johnson <JohnsonM20@lsrhs.net>
2020-02-11 20:57:26 -0500

    Initial Commit

---
+++ TuftsTodayApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TuftsTodayApp
+//
+//  Created by Matthew Johnson on 2/11/20.
+//  Copyright © 2020 Matthew Johnson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
